05bce8dbc7a804ecf4b0a557dc7e084bdcea3673
Usama Rashid Butt <usamarashid.work@gmail.com>
2023-08-04 03:53:27 +0500

    FEAT: add Product Details page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import WomanImg from "../img/woman_hero.png";
 import { Link } from "react-router-dom";
 const Hero = () => {
   return (
-    <section className='bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
+    <section className=' h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
       <div className='container mx-auto flex justify-around h-full'>
         {/* text  */}
         <div className='flex flex-col justify-center'>
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { CartContext } from "../contexts/CartContext";
+import { ProductContext } from "../contexts/ProductContext";
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
+  const { id } = useParams();
+  const { products } = useContext(ProductContext);
+  const { addToCart } = useContext(CartContext);
+
+  const product = products.find((item) => {
+    return item.id === parseInt(id);
+  });
+  if (!product) {
+    return (
+      <section className='h-screen flex justify-center items-center'>
+        Loading!...
+      </section>
+    );
+  }
+  const { title, price, description, image } = product;
+  return (
+    <section className='pt-32 pb-12 lg:py-32 h-screen flex items-center '>
+      <div className='container mx-auto'>
+        {/* image */}
+        <div className='flex flex-col lg:flex-row items-center'>
+          <div className='flex flex-1 justify-center items-center mb-8 lg:mb-0'>
+            <img
+              className='max-w-[200px] lg:max-w-sm '
+              src={image}
+              alt=''
+            ></img>
+          </div>
+          {/* text */}
+          <div className='flex-1 items-center lg:text-left'>
+            <h1 className='text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0'>
+              {title}
+            </h1>
+            <div className='text-xl text-red-500 font-medium mb-6'>
+              Rs {price}
+            </div>
+            <p className='mb-8 '>{description}</p>
+            <button
+              onClick={() => addToCart(product, product.id)}
+              className='bg-primary text-white py-4 px-8 '
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProductDetails;
